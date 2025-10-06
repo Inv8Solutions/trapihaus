44d35b809a74aad55d37a4aff67184e7e45b5634
Harpoon <orillosdominique@gmail.com>
2025-10-06 22:21:49 +0800

    added footer

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -4,16 +4,18 @@ import Navbar from "../components/layout/Navbar";
 import ContactHeader from "./ContactHeader";
 import SendAMessage from "./SendAMessage";
 import Contacts from "./Contacts";
+import Footerr from "../components/layout/Footerr";
 
 export default function Contact() {
   return (
     <>
       <Navbar />
-      <main className="pb-20">
+      <main className="pb-20 mb-20">
         <ContactHeader />
         <SendAMessage />
         <Contacts />
       </main>
+      <Footerr />
     </>
   );
 }
