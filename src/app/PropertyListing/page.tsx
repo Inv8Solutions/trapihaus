20795c0eaba97c1d14fe37473a913918534cee1b
Larkaholic <orillosdominique@gmail.com>
2025-10-10 09:51:09 +0800

    adde property listing

---
+++ src/app/PropertyListing/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import NavBar from "../components/layout/Navbar";
+
+export default function PropertyListingPage() {
+    return (
+        <>
+        <NavBar />
+        <main>
+            
+        </main>
+        </>        
+    );
+}
